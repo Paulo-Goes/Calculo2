d5dd6b51d6fc78cfc24db81ba3218ce25c389153
GEWPTHEKILLERSM <paulosergiolobogoes@gmail.com>
2023-09-25 17:56:47 -0300

    Tentativa

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,13 @@
-import javax.swing.JOptionPane;
-import java.util.function.Function;
-
 public class Main {
     public static void main(String[] args) {
-        String in = JOptionPane.showInputDialog("Digite a função (por exemplo, x^2): ");
-        double a = Double.parseDouble(JOptionPane.showInputDialog("Digite o limite inferior da integral: "));
-        double b = Double.parseDouble(JOptionPane.showInputDialog("Digite o limite superior da integral: "));
-        int inter = Integer.parseInt(JOptionPane.showInputDialog("Digite o número de intervalos para a aproximação: "));
+        // Sample user input as a string
+        String userInput = "-x^2 + 4";
 
-        Function<Double, Double> funcao = x -> seila(in, x);
+        double a = 0.0; // Inferior limit
+        double b = 1.0; // Superior limit
+        int n = 20; // Number of intervals for approximation
 
-        double larg = (b - a) / inter;
-
-        double result = 0.0;
-
-        StringBuilder resultText = new StringBuilder("<html><table><tr><th>Intervalo</th><th>X1</th><th>Área Parcial</th><th>Resultado Parcial</th></tr>");
-
-        for (int i = 0; i < inter; i++) {
-            double x1 = a + i * larg;
-            double area = funcao.apply(x1) * larg;
-            result += area;
-
-            resultText.append(String.format("<tr><td>%d</td><td>%.2f</td><td>%.2f</td><td>%.2f</td></tr>", (i + 1), x1, area, result));
-        }
-
-        resultText.append("</table><br>Resultado da integral definida: ").append(result).append("</html>");
-
-        JOptionPane.showMessageDialog(null, resultText.toString());
-    }
-
-    public static double seila(String expression, double x) {
-        return -(Math.pow(x, 2)) + 4;
+        double result = Riemann.calculateRiemannSum(userInput, a, b, n);
+        System.out.println("Approximated integral: " + result);
     }
 }
---
+++ src/Riemann.java
@@ -0,0 +1,35 @@
+import de.congrace.exp4j.Calculable;
+import de.congrace.exp4j.ExpressionBuilder;
+
+public class Riemann {
+
+    public static double calculateRiemannSum(String expression, double a, double b, int n) {
+        double delta_x = (b - a) / n; // Width of each subinterval
+        double sum = 0.0;
+
+        try {
+            Calculable calc = new ExpressionBuilder(expression)
+                    .withVariable("x", 0.0) // You can use any initial value for the variable
+                    .build();
+
+            for (int i = 0; i < n; i++) {
+                double x_i = a + i * delta_x; // Left endpoint of subinterval
+                double x_i_plus_1 = a + (i + 1) * delta_x; // Right endpoint of subinterval
+
+                // Bind the current value of 'x' to the expression
+                calc.setVariable("x", (x_i + x_i_plus_1) / 2);
+
+                double aux = calc.calculate() * delta_x;
+                // Evaluate the expression and add it to the sum
+                sum += aux;
+
+                System.out.println("Subinterval " + (i + 1) + ": " + aux);
+
+            }
+        } catch (Exception e) {
+            System.err.println("Invalid expression: " + expression);
+        }
+
+        return sum;
+    }
+}
